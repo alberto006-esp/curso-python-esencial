57f4905a103f8401c8816442ffd8f8cea99ac0a1
alberto006-esp <albertosuarezolvera@gmail.com>
2022-06-29 13:53:25 +0200

    Entrar

---
+++ variables.py
@@ -0,0 +1,17 @@
+# String
+nombre = "Ana"
+
+#Entero(int) 
+edad= 20
+
+#Decimal/flotante
+estatura = 1.60
+
+#Boolean
+positivo = True
+negativo = False
+
+print(type(nombre))
+print(type(edad))
+print(type(estatura))
+print(type(positivo))
