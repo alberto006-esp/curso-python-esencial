d19da847c79c793566f79c3fb189e2e44a65ba28
alberto006-esp <albertosuarezolvera@gmail.com>
2022-06-30 23:06:02 +0200

    ciclo while

---
+++ ciclo_while.py
@@ -0,0 +1,6 @@
+i = 1
+while i <= 5: # Es importante que el ciclo while siempre tenga una instrucción que lo detenga sino se ejecutara infinitamente.
+    print(i)
+    i += 1
+    if i == 3:
+        break
